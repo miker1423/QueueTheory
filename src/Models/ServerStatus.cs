92e08b60d5175b34db319a64d2542de40709edfe
eldelcesar <cesarjr.espinosa@gmail.com>
2018-11-06 00:27:57 -0600

    Formateo de string de status

--- src/Models/ServerStatus.cs
+++ src/Models/ServerStatus.cs
@@ -8,10 +8,17 @@ namespace TcpServer.Models
         public int ServedClients { get; set; }
         public int PendingRequests { get; set; }
         public TimeSpan AvarageWaitTime { get; set; }
+        public TimeSpan AvarageArrivalTime { get; set; }
         public float RPS { get; set; }
         public int Workers { get; set; }
 
         public override string ToString()
-            => $"W: {Workers}, RC: {ReceivedClients}, SC: {ServedClients}, PR: {PendingRequests}";
+            =>  "\nTAQUERIA DON MIGUELITO\n" +
+                $"Han llegado {ReceivedClients} clientes a la taquería el día de hoy.\n" +
+                $"En promedio, llegan clientes cada {AvarageArrivalTime.Seconds} segundos.\n" +
+                $"Hay {Workers} meseros disponibles para atender a los {PendingRequests} clientes que esperan en la fila.\n" + 
+                $"En promedio, cada cliente espera {AvarageWaitTime.Seconds} segundos para ser atendido.\n" + 
+                $"De esta forma, la Taquería Don Miguelito puede procesar {RPS} pedidos por segundo (nada mal, eh).\n" + 
+                $"Ya se les ha dado una rica comida a {ServedClients} clientes el dia de hoy.\n";
     }
 }
